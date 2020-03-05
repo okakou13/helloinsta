c9b7cf9cdf696706efcf899664337832cf52e3a3
OKDKO <okakou100@gmail.com>
2020-03-05 10:17:09 +0900

    Initial Commit

---
+++ instaSample2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  instaSample2
+//
+//  Created by 岡田昂典 on 2020/03/05.
+//  Copyright © 2020 Kosuke Okada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
